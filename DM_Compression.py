ed9d0dda6a264930621f758eda49f7b86a1c2bfa
Egeyae <cossec.elouan@gmail.com>
2024-03-14 11:25:06 +0100

    Corrections et tests ajout des tests pour le padding correction de DM_Compression

--- DM_Compression.py
+++ DM_Compression.py
@@ -12,22 +12,22 @@ def load(filename):
 
 def psnr(original, compressed):
     mse = np.mean((original.astype(int) - compressed) ** 2)
+    if mse == 0:
+        return 100
     max_pixel = 255.0
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
 
-#Question1
-
-mat_test=np.zeros([15,21,3],dtype=np.uint8)
+# QUESTION 1
 
 def remplissage(matrice):
     '''fonction qui permet de remplir une matrice de lignes et de colonnes jusqu'à ce que ses dimensions soient des multiples de 4'''
 
-    reste1=mat_test.shape[0]%4
+    reste1=matrice.shape[0]%4
     if reste1!=0:
         lignes_a_ajouter=4-reste1
     
-    reste2=mat_test.shape[1]%4
+    reste2=matrice.shape[1]%4
     if reste2!=0:
         colonnes_a_ajouter=4-reste2
 
@@ -38,9 +38,8 @@ def remplissage(matrice):
 
     return matrice2
 
-print(remplissage(mat_test).shape)
 
-def reset_remplissage(matrice2,lignes,colonnes):
+def reset_remplissage(matrice2,lignes, colonnes):
     '''fonction pour annuler le remplissage de la matrice et revenir à la matrice originale'''
 
     matrice=np.zeros([lignes,colonnes,3],dtype=np.uint8)
@@ -48,4 +47,26 @@ def reset_remplissage(matrice2,lignes,colonnes):
 
     return matrice
 
-print(reset_remplissage(mat_test,15,21).shape)
+# QUESTION 2
+
+def fragment_4x4(image_array):
+    assert image_array.shape[0]%4 == 0 and image_array.shape[1]%4 == 0, "L'image n'est pas divisible en blocs de 4x4"
+
+    frag_list = []
+
+    for i in range(0, image_array.shape[0], 4):
+        frag_list.append([])
+        for j in range(0, image_array.shape[1], 4):
+            frag_list[-1].append(image_array[i:i+4, j:j+4, :3].copy())
+
+    return frag_list
+
+def defragment_4x4(array_list):
+    defrag_array = np.empty([len(array_list)*4, len(array_list[0])*4, 3], dtype=np.uint8)
+
+    for i in range(len(array_list)):
+        for j in range(len(array_list[0])):
+            defrag_array[i*4:i*4+4, j*4:j*4+4] = array_list[i][j]
+
+    return defrag_array
+    
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-from DM_COmpression_Elouan import fragment_4x4, defragment_4x4, load, psnr
+from DM_Compression import fragment_4x4, defragment_4x4, load, psnr, remplissage, reset_remplissage
+import numpy as np
+
 
 def ok(msg):
     print(f"\u001b[32m[OK]\u001b[0m {msg}")
@@ -24,15 +26,47 @@ def test_fragmentation():
     try:
         test_psnr = psnr(test_img, test_defrag)
     except Exception as e:
-        fail(f"Failed to compute PSNR: {e}")
+        fail(f"Failed to compute PSNR for frag: {e}")
         total += 1
     else:
         if test_psnr != 100:
-            fail("Images are different ! PSNR != 100")
+            fail("Images are different for fragmentation ! PSNR != 100")
             total += 1
     
     if total == 0:
-        ok("Fragementation works fine")
+        ok("Fragmentation works fine")
+
+def test_padding():
+    test_mat = np.zeros([15, 21, 3], dtype=np.uint8)
+    total = 0
+
+    try:
+        mat_pad = remplissage(test_mat)
+    except Exception as e:
+        fail(f"Failed to add padding: {e}")
+        total += 1
+    
+    try:
+        mat_unpad = reset_remplissage(mat_pad, 15, 21)
+    except Exception as e:
+        fail(f"Failed to remove padding: {e}")
+        total += 1
+
+    try:
+        test_psnr = psnr(test_mat, mat_unpad)
+    except Exception as e:
+        fail(f"Failed to compute PSNR for padding: {e}")
+        total += 1
+    else:
+        if test_psnr != 100:
+            fail("Arrays are different for padding ! PSNR != 100")
+            total += 1
+    
+    if total == 0:
+        ok("Padding works fine")
+
+
 
 if __name__ == "__main__":
     test_fragmentation()
+    test_padding()
